06ceb535e2d1b67140b8620e93ecde6b828468d0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-08 16:00:44 +0530

    Initial Commit

---
+++ quanta/quantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  quantaApp.swift
+//  quanta
+//
+//  Created by Krisna Pranav on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct quantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
